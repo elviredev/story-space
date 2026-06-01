a9e82b22f6aa1cd5f4b8bd62bdb1e4ff60fde638
elviredev <sandrine.rodriguez@hotmail.com>
2026-06-01 20:56:44 +0200

    📜 Scroll to top automatique

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import { Header, Navbar, Footer } from "@/components"
+import { useEffect } from "react"
 
 const HomeLayout = () => {
+  // scroll to top
+  const location = useLocation()
+  // écouter tout changement sur "location"
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
   return <>
     <Header />
     <Navbar />
